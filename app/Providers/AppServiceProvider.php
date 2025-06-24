1576aeb1457c8f73b55ebcdc4c4f36e7d81c2455
ChairulIkhsan <chairulikhsan23@gmail.com>
2025-06-24 23:13:12 +0700

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use App\Models\RentalItem;
-use App\Observers\RentalItemObserver;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RentalItem::observe(RentalItemObserver::class);
+       //
     }
 }
